9e5f1912c64d75fb307ace3d4cb90f6e82347011
Rooster <othmanmuhidean@gmail.com>
2020-02-29 23:25:08 -0800

    successfully created a config file that will allow environment variables to be used.

--- models/ticker.js
+++ models/ticker.js
@@ -1,7 +1,28 @@
 module.exports = function(sequelize, DataTypes) {
-    var Ticker = sequelize.define("ticker", {
-        name: DataTypes.STRING,
-        symbol: DataTypes.STRING
+    const Ticker = sequelize.define("tickers", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        symbol: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        }
     });
+
+    Ticker.associate = function(models) {
+        // We're saying that a Post should belong to an Author
+        // A Post can't be created without an Author due to the foreign key constraint
+        Ticker.belongsTo(models.marketIndex, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
     return Ticker;
 };
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3000
+const PORT = process.env.DB_PORT || 3306
 const db = require('./models')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('assets'))
 
@@ -13,7 +15,7 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => res.render('home'))
 
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
