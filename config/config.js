745a4b88855414c4b59472986cd564e5945ef3bd
Marc Fernandez <mfernandez225@gmail.com>
2020-03-07 00:59:45 -0800

    heroku

--- config/config.js
+++ config/config.js
@@ -9,21 +9,8 @@ module.exports = {
         port: process.env.DB_PORT,
         dialect: 'mysql'
     },
-    test: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql'
-    },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql'
-
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres'
     }
 };
