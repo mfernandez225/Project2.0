16e413dcc0a9ee9a1596d42069a429bef0c2f3f8
Marc Fernandez <mfernandez225@gmail.com>
2020-03-07 01:25:10 -0800

    read port from env

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
-// eslint-disable-next-line no-undef
-const PORT = 3000
 const db = require('./models')
+const process = require("process")
+
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-
 app.use(express.static('assets'))
-
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.render('home'))
 require("./routes/api-routes.js")(app);
 
-/* The {force:true} will create a new table every time, erasing all data. 
-USE THIS INITIALLY, LOAD UP YOUR DATABASE WITH ANY METHOD. 
-
-YOU MAY ONLY CONTINUE WITH THIS WHEN THE DATABASE HAS NO VALUE. 
-*/
 db.sequelize.sync().then(function () {
-    app.listen(PORT, () => {
-        console.log(`Project 2.0 is listening at http://localhost:${PORT}`)
+    app.listen(process.env.PORT || "3000", () => {
+        console.log(`Project 2.0 is listening at http://localhost:${process.env.PORT || "3000"}`)
     })
 })
